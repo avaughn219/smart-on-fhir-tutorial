c6809d8f00c314edad3b450423276e399724ba93
avaughn219 <88851829+avaughn219@users.noreply.github.com>
2024-07-22 15:06:25 -0500

    Update fhir-client-v0.1.12.js
    made the print timeout longer.

--- example-smart-app/lib/js/fhir-client-v0.1.12.js
+++ example-smart-app/lib/js/fhir-client-v0.1.12.js
@@ -17392,7 +17392,7 @@ BBClient.authorize = function(params, errback){
 	      console.log("fhirServiceUrl: " + url);
 	      console.log("launch: " + launch);
 	      console.log("params?: " + params);
-	    }, 3000, urlParam("iss"), urlParam("fhirServiceUrl"), urlParam("launch"), location.search.substr(1));
+	    }, 30000, urlParam("iss"), urlParam("fhirServiceUrl"), urlParam("launch"), location.search.substr(1));
 	}catch(e){
 		console.log("error for iss/fhirServiceUrl/launch: " + e.message);
 	}
